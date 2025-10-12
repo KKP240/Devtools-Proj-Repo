10b0e53d44b458146c23fa340b6f4b0e578253b0
nutthawat063 <66070063@kmitl.ac.th>
2025-10-12 17:31:12 +0700

    test again again back back

--- pettech/pettech/settings.py
+++ pettech/pettech/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gn7yl&ivvmyj9syhs1(69fpm*wgz492(!luw=zskzbm+)+l4!^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
